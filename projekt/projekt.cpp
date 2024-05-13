139e0ed61c1d449670033a73815e650a656e9b14
Paty1355 <94170413+Paty1355@users.noreply.github.com>
2024-05-13 18:11:20 +0200

    Baza danych

--- projekt/projekt.cpp
+++ projekt/projekt.cpp
@@ -11,24 +11,63 @@ static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
     printf("\n");
     return 0;
 }
-int main(int argc, char** argv) {
-    sqlite3 * db;
+
+int main(int argc, char* argv[]) {
+    sqlite3* db;
     char* zErrMsg = 0;
     int rc;
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
-        return(1);
-    }
-    rc = sqlite3_open(argv[1], &db);
+    const char* sql;
+    const char* sql2;
+
+    /* Open database */
+    rc = sqlite3_open("test.db", &db);
+
     if (rc) {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-        return(1);
+        return(0);
     }
-    rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+    else {
+        fprintf(stdout, "Opened database successfully\n");
+    }
+
+    /* Create SQL statement */
+    sql = "CREATE TABLE IF NOT EXISTS COMPANY("  \
+        "ID INT PRIMARY KEY     NOT NULL," \
+        "NAME           TEXT    NOT NULL," \
+        "AGE            INT     NOT NULL," \
+        "ADDRESS        CHAR(50)," \
+        "SALARY         REAL );";
+
+    sql2 = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+        "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
+        "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+        "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
+        "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+        "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
+        "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+        "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql2, callback, 0, &zErrMsg);
+
     if (rc != SQLITE_OK) {
-        (stderr, "SQL error: %s\n", zErrMsg);
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
         sqlite3_free(zErrMsg);
+    }
+    else {
+        fprintf(stdout, "Records created successfully\n");
+    }
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else {
+        fprintf(stdout, "Table created successfully\n");
     }
     sqlite3_close(db);
     return 0;
