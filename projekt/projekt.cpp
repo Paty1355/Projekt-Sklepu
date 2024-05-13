3ad98a9ed82d21f2dfc2f15e1a6ac380b8d3c22d
Paty1355 <94170413+Paty1355@users.noreply.github.com>
2024-05-14 01:19:46 +0200

    Function add - part1

--- projekt/projekt.cpp
+++ projekt/projekt.cpp
@@ -12,63 +12,71 @@ static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
     return 0;
 }
 
+class Abstract {
+public:
+    virtual void add() {};
+    virtual void remove() {};
+};
+
+class Category:public Abstract {
+protected:
+    string categoryName;
+public:
+    Category(string name) :
+        categoryName(name) {}
+
+    virtual void add() override {
+        sqlite3* db;
+        char* zErrMsg = 0;
+        int rc;
+        const char* sql;
+
+        /* Open database */
+        rc = sqlite3_open("shop.db", &db);
+
+        if (rc) fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        else fprintf(stdout, "Opened database successfully\n");
+
+        /* Creating Tables*/
+        sql = "CREATE TABLE IF NOT EXISTS categories("  \
+            "id int AUTO_INCREMENT PRIMARY KEY," \
+            "categoryName VARCHAR(255) NOT NULL UNIQUE);" \
+            "INSERT INTO categories (categoryName) "  \
+            "VALUES ('test') ON DUPLICATE KEY UPDATE categoryName = 'delete';" //\ nie ma w sqlite sprobuj upsert obczaic
+            "DELETE from categories where categoryName='delete';";
+            
+        /* Execute SQL statement */
+        rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+        if (rc != SQLITE_OK) {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }
+        else {
+            fprintf(stdout, "Table created successfully\n");
+        }
+        sqlite3_close(db);
+    }
+    virtual void remove() override {
+
+    }
+};
+class Product : protected Category {
+    int price;
+    string productName;
+
+    //"CREATE TABLE IF NOT EXISTS products("  \
+    //    "id int AUTO_INCREMENT PRIMARY KEY," \
+    //    "productName TEXT NOT NULL," \
+    //    "price int NOT NULL);" \
+
+public:
+    Product(int price, string nameP, string nameC) :
+        price(price), productName(nameP), Category(nameC) {}
+};
+
 int main(int argc, char* argv[]) {
-    sqlite3* db;
-    char* zErrMsg = 0;
-    int rc;
-    const char* sql;
-    const char* sql2;
-
-    /* Open database */
-    rc = sqlite3_open("test.db", &db);
-
-    if (rc) {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        return(0);
-    }
-    else {
-        fprintf(stdout, "Opened database successfully\n");
-    }
-
-    /* Create SQL statement */
-    sql = "CREATE TABLE IF NOT EXISTS COMPANY("  \
-        "ID INT PRIMARY KEY     NOT NULL," \
-        "NAME           TEXT    NOT NULL," \
-        "AGE            INT     NOT NULL," \
-        "ADDRESS        CHAR(50)," \
-        "SALARY         REAL );";
-
-    sql2 = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-        "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
-        "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
-        "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
-        "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-        "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
-        "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
-        "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
-
-
-    /* Execute SQL statement */
-    rc = sqlite3_exec(db, sql2, callback, 0, &zErrMsg);
-
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    }
-    else {
-        fprintf(stdout, "Records created successfully\n");
-    }
-
-    /* Execute SQL statement */
-    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    }
-    else {
-        fprintf(stdout, "Table created successfully\n");
-    }
-    sqlite3_close(db);
+    Category zywnosc("zywnosc");
+    zywnosc.add();
     return 0;
 }
