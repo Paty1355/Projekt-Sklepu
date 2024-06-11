822cad447a42aae3a4397394c6a97d4673f3c89f
anqa12 <anna.grygar12@gmain.com>
2024-06-11 10:54:18 +0200

    invalid input in the menu now works

--- projekt/projekt.cpp
+++ projekt/projekt.cpp
@@ -751,7 +751,20 @@ int main() {
         cout << "3. Exit" << endl;
         cin >> choice;
 
+        while (!(cin >> choice) || choice < 1 || choice > 3) {
+            system("cls");
+            cout << "-------------------MENU---------------------" << endl;
+            cout << "1. Admin" << endl;
+            cout << "2. User" << endl;
+            cout << "3. Exit" << endl;
+            cin >> choice;
+            cout << "Invalid choice. Please select 1, 2 or 3: ";
+            cin.clear();
+            cin.ignore();
+        }
+
         if (choice == 1) {
+            system("cls");
             cout << "Admin password: " << endl;
 
             // Hide password input
@@ -768,6 +781,8 @@ int main() {
                 bool admin_running = true;
                 do {
                     system("cls");
+
+                    admin_running = false;
                     choice = choose_option();
                     switch (choice) {
                     case 1:
@@ -820,8 +835,9 @@ int main() {
                         break;
                     default:
                         cout << "Invalid choice. Please select a valid option." << endl;
-                        //admin_running = false;
-                        choose_option();
+                        admin_running = false;
+                        break;
+                       
                     }
                 } while (admin_running);
             }
@@ -902,11 +918,8 @@ int main() {
         else if (choice == 3) {
             break;
         }
-        else if (choice != 1 && choice != 2 && choice !=3) {
-            cout << "Choose 1, 2, or 3" << endl;
-            cin >> choice;
-            system("pause");
-
+        else {
+            cout << "Choose 1, 2 or 3" << endl;
         }
     } while (choice != 3);
     return 0;
